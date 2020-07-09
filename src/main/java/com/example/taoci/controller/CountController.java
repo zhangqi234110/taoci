f3e62691c3e66bfd9455e12f9a0d158ce4153b79
zhangqi234110 <51742008+zhangqi234110@users.noreply.github.com>
2020-07-09 09:53:47 +0800

    Update CountController.java

--- src/main/java/com/example/taoci/controller/CountController.java
+++ src/main/java/com/example/taoci/controller/CountController.java
@@ -16,7 +16,7 @@ import java.util.Date;
 import java.util.List;
 
 @Controller
-@RequestMapping("/mycount")
+@RequestMapping("/mycount1234")
 public class CountController {
     @Autowired
     private UserService userService;
