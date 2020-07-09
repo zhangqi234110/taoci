bd954b57a1ede8ba6a6894989a9f09bd485b7132
zhangqi234110 <314890945@qq.com>
2020-07-09 09:52:49 +0800

    chuci3

--- src/main/java/com/example/taoci/controller/CountController.java
+++ src/main/java/com/example/taoci/controller/CountController.java
@@ -16,7 +16,7 @@ import java.util.Date;
 import java.util.List;
 
 @Controller
-@RequestMapping("/mycount123")
+@RequestMapping("/mycount")
 public class CountController {
     @Autowired
     private UserService userService;
